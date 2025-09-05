8643153f531cf6a13d6001c5e93b414b8472e921
maxmalato <max.malato@hotmail.com>
2025-09-05 17:01:10 -0300

    Criação da página Artista
    Realizado a configuração para o consumo da API de Artistas

---
+++ ScreenSound.Web/Services/ArtistaAPI.cs
@@ -0,0 +1,19 @@
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class ArtistaAPI
+{
+    private readonly HttpClient _httpClient;
+    public ArtistaAPI(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("API");
+    }
+
+    // Retorna uma coleção de artistas
+    public async Task<ICollection<ArtistaResponse>?> GetArtistasAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
+    }
+}
