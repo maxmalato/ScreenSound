0e1199b2c25414a4f5d386776b6dcd97c7190b0c
maxmalato <max.malato@hotmail.com>
2025-10-02 17:09:01 -0300

    Editar musica

---
+++ ScreenSound.Web/Requests/MusicaRequestEdit.cs
@@ -0,0 +1,4 @@
+﻿namespace ScreenSound.Web.Requests;
+
+public record MusicaRequestEdit(int Id, string Nome, int ArtistaId, int AnoLancamento)
+    : MusicaRequest(Nome, ArtistaId, AnoLancamento);
---
+++ ScreenSound.Web/Services/MusicaAPI.cs
@@ -0,0 +1,45 @@
+﻿using ScreenSound.Web.Requests;
+using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class MusicaAPI
+{
+    private readonly HttpClient _httpClient;
+
+    public MusicaAPI(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("API");
+    }
+
+    // Listar todas as músicas
+    public async Task<ICollection<MusicaResponse>?> GetMusicasAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("musicas");
+    }
+
+    // Listar música por nome
+    public async Task<MusicaResponse?> GetMusicaPorNomeAsync(string nome)
+    {
+        return await _httpClient.GetFromJsonAsync<MusicaResponse>($"musicas/{nome}");
+    }
+
+    // Adicionar nova música
+    public async Task AddMusicaAsync(MusicaRequest musica)
+    {
+        await _httpClient.PostAsJsonAsync("musicas", musica);
+    }
+
+    // Excluir música por ID
+    public async Task DeleteMusicaAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"musicas/{id}");
+    }
+
+    // Editar música
+    public async Task UpdateMusicaAsync(MusicaRequestEdit artista)
+    {
+        await _httpClient.PutAsJsonAsync("musicas/", artista);
+    }
+}
